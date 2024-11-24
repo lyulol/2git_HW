c20d9e070b3a6845f77750021f8f3401fff8eb82
lyulol <lyulol.n@gmail.com>
2024-11-24 15:43:54 +0200

    init commit

---
+++ src/test/java/SearchTests.java
@@ -0,0 +1,18 @@
+import com.codeborne.selenide.WebDriverRunner;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Cookie;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class SearchTests {
+    @Test
+    void successfulSearchTest() {
+        open("https://www.google.com/");
+        WebDriverRunner.getWebDriver().manage().addCookie(new Cookie("SOCS", "CAESHAgBEhJnd3NfMjAyNDExMTQtMF9SQzEaAmVuIAEaBgiAqu"));
+        refresh();
+        $("[name=q]").setValue("selenide").pressEnter();
+        $("[id=search]").shouldHave(text("https://selenide.org"));
+    }
+
+}
